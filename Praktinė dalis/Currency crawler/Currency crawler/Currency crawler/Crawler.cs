72fedf0a64b322940887a864796930255e5cfda9
MrMariu5 <marius.sadreika@gmail.com>
2021-02-04 22:59:24 +0200

    Base pageload completed

--- Praktinė dalis/Currency crawler/Currency crawler/Currency crawler/Crawler.cs
+++ Praktinė dalis/Currency crawler/Currency crawler/Currency crawler/Crawler.cs
@@ -15,17 +15,15 @@
         {
             Client.AddDefaultHeader("Host", "www1.oanda.com");
             Client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0";
-            Client.AddDefaultHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
             Client.AddDefaultHeader("Accept-Language", "en-GB,en;q=0.5");
             Client.AddDefaultHeader("DNT", "1");
             Client.AddDefaultHeader("Connection", "keep-alive");
-            Client.AddDefaultHeader("Upgrade-Insecure-Requests", "1");
         }
 
         public void StartCrawler()
         {
             TryLoadBasePage(out string javaScriptCacheCode);
-            TryLoadCurrencyList(out List<string> currencies);
+            TryLoadCurrencyList(javaScriptCacheCode, out List<string> currencies);
             
         }
 
@@ -33,15 +31,27 @@
         {
             Client.BaseUrl = new Uri(Urls.BasePageUrl, UriKind.Absolute);
             RestRequest request = new RestRequest("", Method.GET);
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            
             IRestResponse response = Client.Execute(request);
             javaScriptCacheCode = RegexFunctions.RegexToString(response.Content, Regexes.JavaScriptCacheCode);
 
             return javaScriptCacheCode != null;
         }
 
-        public bool TryLoadCurrencyList(out List<string> currencies)
+        public bool TryLoadCurrencyList(string javaScriptCacheCode, out List<string> currencies)
         {
+            currencies = new List<string>();
+            Client.BaseUrl = new Uri(Urls.BasePageUrl + $"cache{javaScriptCacheCode}.js", UriKind.Absolute);
+            RestRequest request = new RestRequest("", Method.GET);
+            request.AddHeader("Accept", "*/*");
+            request.AddHeader("Referer", "https://www1.oanda.com/currency/converter/");
+            IRestResponse response = Client.Execute(request);
+            string currencyJson = RegexFunctions.RegexToString(response.Content, Regexes.CurrencyJson);
 
+
+            return false;
         }
     }
 }
--- Praktinė dalis/Currency crawler/Currency crawler/Currency crawler/Regexes.cs
+++ Praktinė dalis/Currency crawler/Currency crawler/Currency crawler/Regexes.cs
@@ -3,5 +3,6 @@
     public static class Regexes
     {
         public static string JavaScriptCacheCode = @"/currency/converter/cache(.*?)\u002ejs";
+        public static string CurrencyJson = @"var\s*cache\s*=\s*(.*?}}});";
     }
 }
